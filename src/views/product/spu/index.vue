6b86eaac4aa918ec98ae6f4da7cd70d935ffdc9d
liujiacheng <3314460284@qq.com>
2026-09-17 17:42:28 +0800

    feat: 优化多个组件的代码格式，提升可读性

--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -1,30 +1,71 @@
 <template>
-    <div>
-        <Category :scene="scene" />
-        <el-card>
-            <el-button @click="scene = 1" type="primary" icon="Plus" :disabled="!categoryStore.c3Id">
-                添加spu属性
-            </el-button>
-            <el-table style="margin: 20px 0" border :data="records">
-                <el-table-column label="序号" width="80px" type="index" align="center"></el-table-column>
+  <div>
+    <Category :scene="scene" />
+    <el-card>
+      <el-button
+        @click="scene = 1"
+        type="primary"
+        icon="Plus"
+        :disabled="!categoryStore.c3Id"
+      >
+        添加spu属性
+      </el-button>
+      <el-table style="margin: 20px 0" border :data="records">
+        <el-table-column
+          label="序号"
+          width="80px"
+          type="index"
+          align="center"
+        ></el-table-column>
 
-                <el-table-column label="SPU名称" prop="spuName"></el-table-column>
-                <el-table-column label="SPU描述" prop="description" show-overflow-tooltip></el-table-column>
-                <el-table-column label="SPU操作">
-                    <!-- row即为已有的spu对象 -->
-                    <template #default="{ row, $index }">
-                        <el-button type="primary" size="small" icon="plus" title="添加SKU"></el-button>
-                        <el-button type="primary" size="small" icon="Edit" title="修改SPU"></el-button>
-                        <el-button type="primary" size="small" icon="View" title="查看SKU列表"></el-button>
-                        <el-button type="primary" size="small" icon="Delete" title="删除SPU"></el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-            <el-pagination v-model:current-page="pageNo" v-model:page-size="pageSize" :page-sizes="[3, 5, 7, 9]"
-                :total="total" layout="prev, pager, next, jumper, ->, sizes, total" background
-                @current-change="getHasSpu" @size-change="getHasSpu"></el-pagination>
-        </el-card>
-    </div>
+        <el-table-column label="SPU名称" prop="spuName"></el-table-column>
+        <el-table-column
+          label="SPU描述"
+          prop="description"
+          show-overflow-tooltip
+        ></el-table-column>
+        <el-table-column label="SPU操作">
+          <!-- row即为已有的spu对象 -->
+          <template #default="{ row, $index }">
+            <el-button
+              type="primary"
+              size="small"
+              icon="plus"
+              title="添加SKU"
+            ></el-button>
+            <el-button
+              type="primary"
+              size="small"
+              icon="Edit"
+              title="修改SPU"
+            ></el-button>
+            <el-button
+              type="primary"
+              size="small"
+              icon="View"
+              title="查看SKU列表"
+            ></el-button>
+            <el-button
+              type="primary"
+              size="small"
+              icon="Delete"
+              title="删除SPU"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="pageSize"
+        :page-sizes="[3, 5, 7, 9]"
+        :total="total"
+        layout="prev, pager, next, jumper, ->, sizes, total"
+        background
+        @current-change="getHasSpu"
+        @size-change="getHasSpu"
+      ></el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -45,17 +86,24 @@ let total = ref<number>(0)
 let records = ref<Records>()
 let categoryStore = useCategoryStore()
 // 监听三级分类id变化，获取当前三级分类下的所有已有的SPU
-watch(() => categoryStore.c3Id, () => {
+watch(
+  () => categoryStore.c3Id,
+  () => {
     pageNo.value = 1
     getHasSpu()
-})
+  },
+)
 // 此方法执行：可以获取某一个三级分类下全部的已有的SPU
 const getHasSpu = async () => {
-    let res: HasSpuResponseData = await reqHasSpu(pageNo.value, pageSize.value, categoryStore.c3Id)
-    if (res.code === 200) {
-        total.value = res.data.total
-        records.value = res.data.records
-    }
+  let res: HasSpuResponseData = await reqHasSpu(
+    pageNo.value,
+    pageSize.value,
+    categoryStore.c3Id,
+  )
+  if (res.code === 200) {
+    total.value = res.data.total
+    records.value = res.data.records
+  }
 }
 </script>
 
